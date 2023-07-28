d7cc0004c446aeed68a6f1e6f7adabbd5a308b7d
Aaron Janusitis <aaronjanusitis@gmail.com>
2023-07-28 16:32:15 -0400

    Completed first 4 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let returnString = '';
+    inputArray = input.split('');
+    for (i = inputArray.length - 1; i >= 0; i--) {
+        returnString += inputArray[i];
+    }
+    return returnString;
 };
 
 // Do not edit below this line
