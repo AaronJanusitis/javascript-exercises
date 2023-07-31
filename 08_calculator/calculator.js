3ea187bf8616ea338551bb5381ec1eaba5f86dcd
Aaron Janusitis <aaronjanusitis@gmail.com>
2023-07-31 16:29:52 -0400

    Completed more excercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	return inputArray.reduce((partialSum, a) => partialSum + a, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+  return inputArray.reduce( (a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let result = num1;
+	for (let i = 1; i < num2; i++) {
+    result = result * num1;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) return 1;
+  let result = 1;
+	for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
