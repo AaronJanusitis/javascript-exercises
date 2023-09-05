2d18976e35dc16110c40fc7a1c66f4c77a8d5faf
Aaron Janusitis <aaronjanusitis@gmail.com>
2023-09-05 16:46:30 -0400

    Completed last Javascript exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,11 +1,13 @@
 const findTheOldest = function(people) {
-    let oldest = "";
-    let earliest = 2023;
-    people.forEach(person => {
-        if (person.yearOfBirth < earliest){
-          oldest = person;
-        }
-       });
+    const oldest = people.sort(function(a, b) {
+      const currentYear = (new Date()).getFullYear();
+      if(!b.yearOfDeath) b.yearOfDeath = currentYear;
+      const lastPerson = a.yearOfDeath - a.yearOfBirth;
+      const nextPerson = b.yearOfDeath - b.yearOfBirth;
+      return lastPerson > nextPerson ? -1 : 1;
+  });
+
+  return oldest[0];
 };
 
 // Do not edit below this line
